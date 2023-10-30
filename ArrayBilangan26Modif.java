e585ba1a6e6058ec1cb8e22dc1dc7522d19fd62a
Sariafauzani <ajas2654@gmail.com>
2023-10-30 09:39:42 +0700

    2

---
+++ ArrayBilangan26Modif.java
@@ -0,0 +1,8 @@
+public class ArrayBilangan26Modif {
+   public static void main(String[] args) {
+    int[] bil; 
+    bil = new int[4];
+    bil[0] = {5, 13, -7, 17};
+    System.out.print(bil[4]);
+}
+} 
---
+++ ArrayBilangan26Modif2.java
@@ -0,0 +1,3 @@
+public class ArrayBilangan26Modif2 {
+    
+}
