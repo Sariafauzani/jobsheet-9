9a247d5620ab96cb6f443fc34ab9abbf6be2dd3d
Sariafauzani <ajas2654@gmail.com>
2023-10-30 10:37:02 +0700

    3

--- ArrayBilangan26Modif.java
+++ ArrayBilangan26Modif.java
@@ -2,7 +2,7 @@ public class ArrayBilangan26Modif {
    public static void main(String[] args) {
     int[] bil; 
     bil = new int[4];
-    bil[0] = {5, 13, -7, 17};
+    bil[4] = {5, 13, -7, 17};
     System.out.print(bil[4]);
 }
 } 
